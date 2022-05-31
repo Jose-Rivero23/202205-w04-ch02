d811858cd9f15370b592abd5b933b22790dcf863
JoseRivero <josearmandorr95@gmail.com>
2022-05-31 17:26:50 +0200

    Initial Commit

---
+++ src/components/form.tsx
@@ -0,0 +1,10 @@
+export function form(userData: Array<object>) {
+  return (
+    <form>
+      <input type="text" name="" id="" />
+      <input type="text" name="" id="" />
+      <input type="checkbox" name="" id="" />
+      <input type="radio" name="" id="" />
+    </form>
+  );
+}
